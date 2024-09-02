92e4e0537a7b0ab3fb4f7466ac25d83e76df6f1b
jwanp <joohwan726@naver.com>
2024-09-02 16:33:29 +0900

    chore: lib,store,components 폴더 추가, store 에는 redux 기본 설정

---
+++ nae-essay/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, UseDispatch, useSelector } from 'react-redux';
+export const store = configureStore({
+    reducer: {},
+});
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
